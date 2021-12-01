dbaa0a064be4216615d433ccc2df2195b64316e9
James Humphries <jhumphries@vqcomms.com>
2021-12-01 14:31:22 +0000

    moved tests around a bit

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -22,22 +22,16 @@ fn count_increments(input: &[usize]) -> usize {
     input.windows(2).filter(|win| win[0] < win[1]).count()
 }
 
-#[cfg(test)]
-mod day1 {
-    use super::find_result_1;
-    use super::find_result_2;
+#[test]
+fn part1() {
+    let vec = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+    let result = find_result_1(&vec);
+    assert_eq!(7, result);
+}
 
-    #[test]
-    fn part1() {
-        let vec = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
-        let result = find_result_1(&vec);
-        assert_eq!(7, result);
-    }
-
-    #[test]
-    fn part2() {
-        let vec = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
-        let result = find_result_2(&vec);
-        assert_eq!(5, result)
-    }
+#[test]
+fn part2() {
+    let vec = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+    let result = find_result_2(&vec);
+    assert_eq!(5, result)
 }
